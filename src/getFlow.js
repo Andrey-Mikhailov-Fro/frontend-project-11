ec7965c1b55fd1fb2a5bf4de9ab7c7cd604e8280
Andrey-Mikhailov-Fro <dark.ace.frost@gmail.com>
2024-06-07 01:59:36 +0300

    refactoring #1.8

--- src/getFlow.js
+++ src/getFlow.js
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 export default (url) => {
   const allOriginsUrl = new URL(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`);
-  return axios.get(allOriginsUrl);
+  return axios.get(allOriginsUrl).catch(() => {
+    throw new Error('rssForm.errors.network');
+  });
 };
